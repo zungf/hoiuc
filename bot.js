f1fcc6f1814672366ac139b39d57ae77b83cd42e
Zino <41270843+zungf@users.noreply.github.com>
2018-11-23 11:37:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,6 @@
 const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
-const fs = require('fs');
-const getYoutubeID = require('get-youtube-id');
-const moment = require('moment');
-const db = require('quick.db');
-const client = new Discord.Client();  
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl');  
-const translate = require('google-translate-api');  
-const UserBlocked = new Set();
-const jimp = require('jimp');  
-const math = require('math-expression-evaluator');
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it');
-const queue = new Map();
-const zalgo = require('zalgolize');  
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
- const dateFormat = require('dateformat');
- const pretty = require('pretty-ms')
-const sWlc = {}
-
-
 
 
 
