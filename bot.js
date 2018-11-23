79b6e270d4071a4836819b858937e74ad04dc225
Zino <41270843+zungf@users.noreply.github.com>
2018-11-23 11:40:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,116 @@
 const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const moment = require('moment');
+const db = require('quick.db');
+const client = new Discord.Client();  
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl');  
+const translate = require('google-translate-api');  
+const UserBlocked = new Set();
+const jimp = require('jimp');  
+const math = require('math-expression-evaluator');
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it');
+const queue = new Map();
+const zalgo = require('zalgolize');  
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+ const dateFormat = require('dateformat');
+ const pretty = require('pretty-ms')
+const sWlc = {}
 
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Wsu Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('guildMemberAdd', member => {
+const YossiF = member.guild.channels.get("505479736236179498");//ايدي الشات
+if(!YossiF) return;
+if(YossiF) {
+YossiF.send(`**..Welcome :dove:   To  OnlyGames :leaves:  ..**`);          
+}
+});
 
 
 
 const prefix = "H"
-const devs = ['299880832745734147' , '299880832745734147' , ''];
-client.on('message', message => {//for dev
-  var argresult = message.content.split(`).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '299880832745734147') return;
 
-if (message.content.startsWith(adminprefix + 'playig')) {
+if (message.content.startsWith(prefix + 'g')) {
   client.user.setGame(argresult);
-    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
-} else
-  if (message.content.startsWith(adminprefix + 'name')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'avatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(${argresult} : تم تغير صورة البوت);
-      } else
-if (message.content.startsWith(adminprefix + 'twitch')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
-    message.channel.sendMessage(تم تغيير تويتش البوت إلى  ${argresult}`)
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
 
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
 
-
+});
 
 
 
